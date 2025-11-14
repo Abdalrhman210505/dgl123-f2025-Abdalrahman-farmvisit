5584cd04917c6c09a486f628bf33d0d8205aae20
AbdalrahmanDEV <aawad@northislandcollege.ca>
2025-11-13 19:37:36 -0800

    Create Skeleton html

--- admin/hours.php
+++ admin/hours.php
@@ -12,3 +12,17 @@ $hours = $stmt -> fetchAll(PDO::FETCH_ASSOC);
 // label
 $days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+        
+    </main>
+</body>
+</html>
