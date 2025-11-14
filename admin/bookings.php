c37132e0526219f306909f559488b4047a02725f
AbdalrahmanDEV <aawad@northislandcollege.ca>
2025-11-13 16:13:04 -0800

    fetch bookings from db onto bookings page

--- admin/bookings.php
+++ admin/bookings.php
@@ -5,4 +5,8 @@ if (!isset($_SESSION["user_id"])) {
     exit;
 }
 require_once("../config/db.php");
+
+
+$stmt = $pdo->query(SELECT * FROM bookings ORDER BY created_at DESC);
+$bookings = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
--- admin/upload_gallery.php
+++ admin/upload_gallery.php
@@ -5,4 +5,5 @@ if (!isset($_SESSION["user_id"])) {
     exit;
 }
 require_once("../config/db.php");
+
 ?>
