c8e99cd45e217a8efeed6aeb28dbbc8247ce3e4e
AbdalrahmanDEV <aawad@northislandcollege.ca>
2025-11-13 16:13:29 -0800

    Create html skeleton

--- admin/bookings.php
+++ admin/bookings.php
@@ -10,3 +10,15 @@ require_once("../config/db.php");
 $stmt = $pdo->query(SELECT * FROM bookings ORDER BY created_at DESC);
 $bookings = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
