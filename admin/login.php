4efae62a3227f1275012f88e2a95e49e0b188e68
AbdalrahmanDEV <aawad@northislandcollege.ca>
2025-11-13 18:35:50 -0800

    Verifying password corresponded with the asscosiative array the query returned a

--- admin/login.php
+++ admin/login.php
@@ -14,3 +14,15 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") { // checks if the user clicked on th
 
     $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
+    if ($user && password_verify($password, $user["password_hash"])) { // checks if the username and passowrd hash translated encrypt again and verify 
+        session_regenerate_id(true);
+        $_SESSION["user_id"] = $user["user_id"];
+        $_SESSION["username"] = $user["username"];
+
+        header("Location: dashboard.php");
+        exit;
+    } else {
+        $error = "Invalid username or password.";
+    }
+}
+?>
