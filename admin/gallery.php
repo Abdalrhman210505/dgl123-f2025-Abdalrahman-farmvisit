ff8a2384d84b677d3a4b98215cb614734841c41e
AbdalrahmanDEV <aawad@northislandcollege.ca>
2025-11-13 16:41:22 -0800

    showing all images of gallery and creating skeleton

--- admin/gallery.php
+++ admin/gallery.php
@@ -5,4 +5,21 @@ if (!isset($_SESSION["user_id"])) {
     exit;
 }
 require_once("../config/db.php");
+
+
+//getting all the images
+$stmt = $pdo -> query("SELECT * FROM gallery_images ORDER BY uploaded_at DESC")
+$images = $stmt -> fetchAll(PDO:: FETCH_ASSOC)
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
