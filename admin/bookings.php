b193e7c6c59af609e6385064cff4c6c653cf6951
AbdalrahmanDEV <aawad@northislandcollege.ca>
2025-11-13 16:20:18 -0800

    Add bookings table to manifest the booking on the screen

--- admin/bookings.php
+++ admin/bookings.php
@@ -16,9 +16,48 @@ $bookings = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Manage Bookings</title>
 </head>
+
 <body>
+    <main>
+        <h1>Manage Bookings</h1>
+
+        <table>
+
+        <tr>
+        <th>ID</th>
+        <th>Visitor Name</th>
+        <th>Email</th>
+        <th>Date</th>
+        <th>Time</th>
+        <th>Party Size</th>
+        <th>Status</th>
+        <th>Actions</th>
+    </tr>
+<?php foreach $bookings as $b ?>
+ <tr>
+        <td><?= $b["booking_id"] ?></td>
+        <td><?= htmlspecialchars($b["visitor_name"]) ?></td> <!-- I have used special chars method to protect the page from xos attacks-->
+        <td><?= htmlspecialchars($b["email"]) ?></td>
+        <td><?= $b["visit_date"] ?></td>
+        <td><?= $b["visit_time"] ?></td>
+        <td><?= $b["party_size"] ?></td>
+        <td><?= $b["status"] ?></td>
+        <td>
+            <a href="update_booking.php?id=<?= $b["booking_id"] ?>&status=confirmed">Confirm</a> |
+            <a href="update_booking.php?id=<?= $b["booking_id"] ?>&status=cancelled">Cancel</a>
+        </td>
+    </tr>
+    <?php endforeach ?>
+
+    
+        </table>
+
+        <p><a href="dashboard.php">⬅ Back to Dashboard</a></p>
+
+
+    </main>
     
 </body>
 </html>
