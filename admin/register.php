feeb2c87f27538cf2c4e3fc9fbc3c0eaeb313730
AbdalrahmanDEV <aawad@northislandcollege.ca>
2025-11-15 14:04:36 -0800

    Create html skeleton for register Page

--- admin/register.php
+++ admin/register.php
@@ -6,3 +6,15 @@ if (!isset($_SESSION["user_id"])) {
 }
 require_once("../config/db.php");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+</head>
+<body>
+    <main>
+    </main>
+</body>
+</html>
