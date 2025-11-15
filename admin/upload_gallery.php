fc2cf9f01e45845ff3369abd1f393d5e0eabbed6
AbdalrahmanDEV <aawad@northislandcollege.ca>
2025-11-15 13:51:11 -0800

    Create html skeleton for upload_gallery

--- admin/upload_gallery.php
+++ admin/upload_gallery.php
@@ -4,6 +4,16 @@ if (!isset($_SESSION["user_id"])) {
     header("Location: login.php");
     exit;
 }
+
 require_once("../config/db.php");
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Upload Image</title>
+</head>
+<body>
+</body>
+</html>
