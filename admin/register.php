2e75fdfef542ffc8f35fb3d06d3f7dbba9e60aef
AbdalrahmanDEV <aawad@northislandcollege.ca>
2025-11-16 18:01:05 -0800

    implement registration form processing with validation

--- admin/register.php
+++ admin/register.php
@@ -1,10 +1,34 @@
 <?php
 session_start();
-if (!isset($_SESSION["user_id"])) {
-    header("Location: login.php");
-    exit;
-}
 require_once("../config/db.php");
+
+$message = "";
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+
+    $username = trim($_POST["username"]); // trim is to remove unwanted spaces or weird symbols
+    $email    = trim($_POST["email"]);
+    $fullName = trim($_POST["full_name"]);
+    $password = $_POST["password"];
+    $confirm  = $_POST["confirm_password"];
+
+    // Making sure that there is no empty fields
+    if ($username === "" || $email === "" || $password === "") {
+        $message = "All fields are required.";
+    }
+    // Check password match
+    elseif ($password !== $confirm) {
+        $message = "Passwords do not match.";
+    } 
+    else {
+
+        // Check if username exists
+        $stmt = $pdo -> prepare("SELECT * FROM users WHERE username = ?");
+        $stmt -> execute([$username]);
+
+        if ($stmt -> rowCount() > 0) {
+            $message = "Username already taken.";
+        } else {
 ?>
 <!DOCTYPE html>
 <html lang="en">
