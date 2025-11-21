73e8a93b28bfbc63288269e15cfbd266155e2eb2
AbdalrahmanDEV <aawad@northislandcollege.ca>
2025-11-20 19:45:46 -0800

    create booking submission handler endpoint
    - Added handlers/submit_booking.php
    - Set JSON response headers
    - Added request method guard (POST only)

---
+++ handlers/submit_booking.php
@@ -0,0 +1,10 @@
+<?php
+header('Content-Type: application/json');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    http_response_code(405);
+    echo json_encode(['success' => false, 'message' => 'Method not allowed']);
+    exit;
+}
+
+require_once __DIR__ . '/../config/db.php';
